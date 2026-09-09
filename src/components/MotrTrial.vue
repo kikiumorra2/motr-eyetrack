689ce6f64d5aec6fb4ee44f4a19bdbe4d60b4821
kikiumorra2 <kikiumorra2@gmail.com>
2026-09-09 16:42:39 -0400

    Update MotrTrial.vue

--- src/components/MotrTrial.vue
+++ src/components/MotrTrial.vue
@@ -123,7 +123,7 @@ export default {
     /** Number of main trials (for the "Sentence i of N" counter) */
     total: { type: Number, required: true },
     listId: { type: [Number, String], default: null },
-	sentenceFontSize:{type: Number, default: 16},
+	sentenceFontSize:{type: Number, default: 18},
   },
   data() {
     return {
