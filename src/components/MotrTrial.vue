710957b0510cfbd3a34cf7b32ee722a484913687
kikiumorra2 <kikiumorra2@gmail.com>
2026-09-09 16:52:10 -0400

    Update MotrTrial.vue make cursor adapt to font size

--- src/components/MotrTrial.vue
+++ src/components/MotrTrial.vue
@@ -42,7 +42,10 @@
 -->
 <template>
   <div>
-    <div class="oval-cursor"></div>
+    <div 
+		class="oval-cursor"
+		:style="{ fontSize: sentenceFontSize+'px' }"
+	></div>
 
     <div class="trial-counter">
       <template v-if="trial.phase === 'practice'">Practice sentence {{ number }}</template>
@@ -411,12 +414,12 @@ button {
   transition: width 0.5s, height 0.5s;
 }
 .oval-cursor.grow.blank {
-  width: 80px;
-  height: 13px;
+  width: 4.444em; //original was 80px for 18px font. 80/18 = 4.444em
+  height: 0.722em; //13/18 = 0.722em
 }
 .oval-cursor.grow {
-  width: 102px;
-  height: 38px;
+  width: 5.667em; //102/18 = 5.667em
+  height: 2.111em; //38/18 = 2.111
   border-radius: 50%;
   box-shadow: 30px 0 8px -4px rgba(255, 255, 255, 0.1),
     -30px 0 8px -4px rgba(255, 255, 255, 0.1);
